92d8778a2969992fcd005db75febcc2d0445fa48
Saravanan Suresh <98247476+SaravananSuresh7@users.noreply.github.com>
2022-02-15 23:44:47 +0530

    Add files via upload

--- Appear.java
+++ Appear.java
@@ -1,8 +1,13 @@
 package week4.day1.assignments;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -10,7 +15,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Appear {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
@@ -25,6 +30,9 @@ public class Appear {
 		}
 		String Buttontext = driver.findElement(By.id("btn")).getText();
 		System.out.println("The button text is " +Buttontext);
+		File source = driver.getScreenshotAs(OutputType.FILE);
+		File dest = new File(".src/main/resources/snaps/Img002.jpg");
+		FileUtils.copyFile(source, dest);
 	}
 
 }
--- Disappear.java
+++ Disappear.java
@@ -1,8 +1,12 @@
 package week4.day1.assignments;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -10,7 +14,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Disappear {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
@@ -18,6 +22,9 @@ public class Disappear {
 		WebDriverWait ExWait = new WebDriverWait(driver,Duration.ofSeconds(10));
 		ExWait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("btn")));
 		boolean Button = driver.findElement(By.xpath("//button[@id = 'btn']")).isDisplayed();
+		File source = driver.getScreenshotAs(OutputType.FILE);
+		File dest = new File(".src/main/resources/snaps/Img003.jpg");
+		FileUtils.copyFile(source, dest);
 		if (Button != true) {
 			System.out.println("The button has Disappeared");
 		} else {
